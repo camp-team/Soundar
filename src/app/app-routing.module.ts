c4b6073e6731b0c24958b8f780be677076ac1157
shhymmt <mail@shhymmt.com>
2020-10-12 17:37:23 +0900

    feat(terms-component): add terms component #10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'terms',
+    loadChildren: () => import('./terms/terms.module').then(m => m.TermsModule)
+
+  }
+];
 
 @NgModule({
   imports: [
