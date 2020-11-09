84c0d81eed869130fcf8d21a0bb475fa90caf166
shhymmt <mail@shhymmt.com>
2020-11-09 10:57:50 +0900

    feat(user-shell): add component

--- src/app/user-shell/user-shell/user-shell.component.spec.ts
+++ src/app/user-shell/user-shell/user-shell.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoggedInShellComponent } from './logged-in-shell.component';
+import { UserShellComponent } from './user-shell.component';
 
-describe('LoggedInShellComponent', () => {
-  let component: LoggedInShellComponent;
-  let fixture: ComponentFixture<LoggedInShellComponent>;
+describe('UserShellComponent', () => {
+  let component: UserShellComponent;
+  let fixture: ComponentFixture<UserShellComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoggedInShellComponent ]
+      declarations: [ UserShellComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoggedInShellComponent);
+    fixture = TestBed.createComponent(UserShellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
