8ad1a02ec7db8c3acdb8ad91ccf16e97ba0facc3
shhymmt <mail@shhymmt.com>
2020-11-09 10:41:58 +0900

    feat(postcomponent): 保存投稿ボタンの修正

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
+
+/**
+ * ここからはこのコンポーネントで使う型の定義を書く
+ */
 export class PostComponent implements OnInit {
+  isActive: boolean; // isActiveにはboolean（真偽値）がはいる
 
-
-
-  form = this.fb.group ({
+  // thisは、プログラム内のどこでもいつでも単体で利用できる特別な存在です。
+  // 単独で呼び出せるのが関数、変数や値につけて呼び出すのがメソッド
+  form: FormGroup = this.fb.group({
     content: [''],
-    title: ['']
+    title: [''],
   });
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {} // PostComponentクラスの型の利用
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  // tslint:disable-next-line: typedef
+  active() {
+    this.isActive = !this.isActive;
   }
-
 }
