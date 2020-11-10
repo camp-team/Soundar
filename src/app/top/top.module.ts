e5abbc4d619b8f563f626067beae7498f9944583
shhymmt <mail@shhymmt.com>
2020-11-10 16:25:48 +0900

    feat(hero.component) : add main visual #120

--- src/app/top/top.module.ts
+++ src/app/top/top.module.ts
@@ -5,12 +5,8 @@ import { TopRoutingModule } from './top-routing.module';
 import { TopComponent } from './top.component';
 import { HeroComponent } from '../hero/hero.component';
 
-
 @NgModule({
   declarations: [TopComponent, HeroComponent],
-  imports: [
-    CommonModule,
-    TopRoutingModule
-  ]
+  imports: [CommonModule, TopRoutingModule],
 })
-export class TopModule { }
+export class TopModule {}
