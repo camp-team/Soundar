3d83dbf76eaaf6ba3adb9902f7edefc020e1b965
shhymmt <mail@shhymmt.com>
2020-11-11 11:55:58 +0900

    memo-cardコンポーネント追加 #121

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { MemoCardComponent } from './memo-card/memo-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MemoCardComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/memo-card/memo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memo-card',
+  templateUrl: './memo-card.component.html',
+  styleUrls: ['./memo-card.component.scss']
+})
+export class MemoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
