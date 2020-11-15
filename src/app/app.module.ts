0926e5084438450c5fef6e78907229c2413dd600
shhymmt <mail@shhymmt.com>
2020-11-15 14:53:27 +0900

    fix(top画面) レスポンシブ対応を意識したコードに修正 #163

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MemoCardComponent } from './memo-card/memo-card.component';
 
 @NgModule({
   declarations: [AppComponent],
