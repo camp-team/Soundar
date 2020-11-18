2d7b5228a1a61c91899a5950f319a06e2b291c86
shhymmt <mail@shhymmt.com>
2020-11-18 13:42:56 +0900

    コンポーネント作成まで #125

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { RecentMovieComponent } from './recent-movie/recent-movie.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
-  declarations: [AppComponent,   ],
+  declarations: [AppComponent, RecentMovieComponent, MovieCardComponent,   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
