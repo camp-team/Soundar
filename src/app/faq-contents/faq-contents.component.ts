d9e2234cb3bc612646cf2aa60f386cd05112c798
shhymmt <mail@shhymmt.com>
2020-11-20 17:52:50 +0900

    アコーディオン実装まで #132

--- src/app/faq-contents/faq-contents.component.ts
+++ src/app/faq-contents/faq-contents.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-faq-contents',
   templateUrl: './faq-contents.component.html',
-  styleUrls: ['./faq-contents.component.scss']
+  styleUrls: ['./faq-contents.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state(
+        'true',
+        style({
+          height: '*',
+          opacity: 1
+        })
+      ),
+      state(
+        'false',
+        style({
+          height: 0,
+          opacity: 0
+        })
+      ),
+      transition('true <=> false', [
+        animate('230ms cubic-bezier(0.35, 0, 0.25, 1)')
+      ])
+    ])
+  ]
 })
 export class FaqContentsComponent implements OnInit {
-
+  isOpen = false;
   constructor() { }
 
   ngOnInit(): void {
