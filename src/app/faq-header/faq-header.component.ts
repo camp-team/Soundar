fc4555fb5c623a30ba097bd7b87a410e6fbe0def
shhymmt <mail@shhymmt.com>
2020-11-20 12:36:57 +0900

    faq-headerコンポーネント作成 #132

---
+++ src/app/faq-header/faq-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-header',
+  templateUrl: './faq-header.component.html',
+  styleUrls: ['./faq-header.component.scss']
+})
+export class FaqHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/faq/faq.module.ts
+++ src/app/faq/faq.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FaqRoutingModule } from './faq-routing.module';
 import { FaqComponent } from './faq.component';
+import { FaqHeaderComponent } from '../faq-header/faq-header.component';
 
 
 @NgModule({
-  declarations: [FaqComponent],
+  declarations: [FaqComponent, FaqHeaderComponent],
   imports: [
     CommonModule,
     FaqRoutingModule
