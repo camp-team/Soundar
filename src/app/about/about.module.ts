74289368feb498a0d28b93b2d0c19781fe9b31e7
shhymmt <mail@shhymmt.com>
2020-11-24 13:31:37 +0900

    contents部分の作成開始 #127

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -5,13 +5,15 @@ import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { AboutHeaderComponent } from '../about-header/about-header.component';
 import { AboutContentsComponent } from '../about-contents/about-contents.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [AboutComponent, AboutHeaderComponent, AboutContentsComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    SharedModule,
   ]
 })
 export class AboutModule { }
