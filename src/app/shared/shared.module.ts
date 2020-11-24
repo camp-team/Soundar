6553cacfc83adbf8b9cc40047a8784de17868bc0
shhymmt <mail@shhymmt.com>
2020-11-24 13:01:35 +0900

    sharedモジュール追加まで #127

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { HeaderGuideComponent } from '../header-guide/header-guide.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderGuideComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
-  ]
+  ],
+  exports: [HeaderGuideComponent]
 })
 export class SharedModule { }
