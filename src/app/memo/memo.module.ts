d2c8526b5f7fe43d73535a1553ea0ec656a939f0
shhymmt <mail@shhymmt.com>
2020-12-03 17:46:34 +0900

    カテゴリーカードコンポーネント作成まで #143

--- src/app/memo/memo.module.ts
+++ src/app/memo/memo.module.ts
@@ -6,10 +6,11 @@ import { MemoComponent } from './memo.component';
 import { DetailHeaderComponent } from '../detail-header/detail-header.component';
 import { ProfileCardComponent } from '../profile-card/profile-card.component';
 import { MemoContentsComponent } from '../memo-contents/memo-contents.component';
+import { CategoryCardComponent } from '../category-card/category-card.component';
 
 
 @NgModule({
-  declarations: [MemoComponent, DetailHeaderComponent, ProfileCardComponent, MemoContentsComponent],
+  declarations: [MemoComponent, DetailHeaderComponent, ProfileCardComponent, MemoContentsComponent, CategoryCardComponent],
   imports: [
     CommonModule,
     MemoRoutingModule
