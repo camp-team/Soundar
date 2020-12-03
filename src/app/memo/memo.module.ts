89af78a446c4bfc7c7d24d5b49dd47b0bc18381a
shhymmt <mail@shhymmt.com>
2020-12-03 15:00:26 +0900

    feat(profile-card): 詳細画面のプロフィールカード追加 #140

--- src/app/memo/memo.module.ts
+++ src/app/memo/memo.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MemoRoutingModule } from './memo-routing.module';
 import { MemoComponent } from './memo.component';
 import { DetailHeaderComponent } from '../detail-header/detail-header.component';
+import { ProfileCardComponent } from '../profile-card/profile-card.component';
 
 
 @NgModule({
-  declarations: [MemoComponent, DetailHeaderComponent],
+  declarations: [MemoComponent, DetailHeaderComponent, ProfileCardComponent],
   imports: [
     CommonModule,
     MemoRoutingModule
