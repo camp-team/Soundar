b70ec13b10df38d5402313328e317d7bff738099
shhymmt <mail@shhymmt.com>
2020-12-04 17:13:14 +0900

    著者カードの作成まで #143

--- src/app/memo/memo.module.ts
+++ src/app/memo/memo.module.ts
@@ -8,10 +8,11 @@ import { ProfileCardComponent } from '../profile-card/profile-card.component';
 import { MemoContentsComponent } from '../memo-contents/memo-contents.component';
 import { CategoryCardComponent } from '../category-card/category-card.component';
 import { ContentsListComponent } from '../contents-list/contents-list.component';
+import { AuthorComponent } from '../author/author.component';
 
 
 @NgModule({
-  declarations: [MemoComponent, DetailHeaderComponent, ProfileCardComponent, MemoContentsComponent, CategoryCardComponent, ContentsListComponent],
+  declarations: [MemoComponent, DetailHeaderComponent, ProfileCardComponent, MemoContentsComponent, CategoryCardComponent, ContentsListComponent, AuthorComponent],
   imports: [
     CommonModule,
     MemoRoutingModule
