b4c3ec605f97a8b51bf51b95db8221e5d1918ed2
shhymmt <mail@shhymmt.com>
2020-12-11 12:04:09 +0900

    プライバシーポリシーのヘッダー部分まで

--- src/app/privacy/privacy.module.ts
+++ src/app/privacy/privacy.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PrivacyRoutingModule } from './privacy-routing.module';
 import { PrivacyComponent } from './privacy.component';
+import { PrivacyHeaderComponent } from '../privacy-header/privacy-header.component';
 
 
 @NgModule({
-  declarations: [PrivacyComponent],
+  declarations: [PrivacyComponent, PrivacyHeaderComponent],
   imports: [
     CommonModule,
     PrivacyRoutingModule
