b731ac1bec3fe07408bbe11bfe1bfd968a7afad6
shhymmt <mail@shhymmt.com>
2020-12-11 13:42:51 +0900

    【コンポーネント設計】プライバシーポリシー画面作成（privacy-component） #151

---
+++ src/app/privacy-contents/privacy-contents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-contents',
+  templateUrl: './privacy-contents.component.html',
+  styleUrls: ['./privacy-contents.component.scss']
+})
+export class PrivacyContentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/privacy/privacy.module.ts
+++ src/app/privacy/privacy.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PrivacyRoutingModule } from './privacy-routing.module';
 import { PrivacyComponent } from './privacy.component';
 import { PrivacyHeaderComponent } from '../privacy-header/privacy-header.component';
+import { SharedModule } from '../shared/shared.module';
+import { PrivacyContentsComponent } from '../privacy-contents/privacy-contents.component';
 
 
 @NgModule({
-  declarations: [PrivacyComponent, PrivacyHeaderComponent],
+  declarations: [PrivacyComponent, PrivacyHeaderComponent, PrivacyContentsComponent],
   imports: [
     CommonModule,
-    PrivacyRoutingModule
+    PrivacyRoutingModule,
+    SharedModule
   ]
 })
 export class PrivacyModule { }
