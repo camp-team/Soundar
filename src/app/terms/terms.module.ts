9b0dc90ba7d4749f0a1b0fade0ddee6b35008a1c
shhymmt <mail@shhymmt.com>
2020-12-11 15:27:15 +0900

    【コンポーネント設計】利用規約画面の作成（terms-component） #152

--- src/app/terms/terms.module.ts
+++ src/app/terms/terms.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { TermsRoutingModule } from './terms-routing.module';
 import { TermsComponent } from './terms.component';
+import { SharedModule } from '../shared/shared.module';
+import { TermsHeaderComponent } from '../terms-header/terms-header.component';
+import { TermsContentsComponent } from '../terms-contents/terms-contents.component';
 
 
 @NgModule({
-  declarations: [TermsComponent],
+  declarations: [TermsComponent, TermsHeaderComponent, TermsContentsComponent],
   imports: [
     CommonModule,
-    TermsRoutingModule
+    TermsRoutingModule,
+    SharedModule
   ]
 })
 export class TermsModule { }
