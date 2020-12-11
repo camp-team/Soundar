21ea5a30de02515e36045d8ef1016ecce167ebf3
shhymmt <mail@shhymmt.com>
2020-12-11 11:51:34 +0900

    ルーティングまで

--- src/app/privacy/privacy-routing.module.ts
+++ src/app/privacy/privacy-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrivacyComponent } from './privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PrivacyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
