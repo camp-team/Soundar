c5d1c42f2f144c5cd57747e3a23c23e7cd0400c3
shhymmt <mail@shhymmt.com>
2020-12-14 17:01:13 +0900

    【コンポーネント設計】詳細画面のマガジンの実際のページ画面 #148

---
+++ src/app/magazine-viewer/magazine-viewer-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MagazineViewerComponent } from './magazine-viewer.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MagazineViewerComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MagazineViewerRoutingModule { }
---
+++ src/app/magazine-viewer/magazine-viewer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MagazineViewerRoutingModule } from './magazine-viewer-routing.module';
+import { MagazineViewerComponent } from './magazine-viewer.component';
+
+
+@NgModule({
+  declarations: [MagazineViewerComponent],
+  imports: [
+    CommonModule,
+    MagazineViewerRoutingModule
+  ]
+})
+export class MagazineViewerModule { }
