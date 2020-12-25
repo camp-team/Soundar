c5ab4ab2838116516e0f92a0bc7a35cf57d6d863
shhymmt <mail@shhymmt.com>
2020-12-25 13:02:20 +0900

    #191 チュートリアル部分まで

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { auth } from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    private db: AngularFirestore
+  ) { }
+
+  login() {
+    const provider = new auth.GoogleAuthProvider();
+    provider.setCustomParameters({prompt: 'select_acount'});
+    this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+}
