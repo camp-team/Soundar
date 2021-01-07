8a8acbd49f29cb35b9f753b8e995db1a530d026a
shhymmt <mail@shhymmt.com>
2021-01-07 11:31:14 +0900

    #193 コメントアウトを削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,10 +22,4 @@ export const createUser = functions.auth.user().onCreate((user) => {
 export const deleteUser = functions.auth.user().onDelete((user) => {
   return db.doc('users/${user.uid}').delete();
 });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
