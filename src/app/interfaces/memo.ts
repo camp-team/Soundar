f2b3f56ab3352984c45744a782a27ccaedf865a9
shhymmt <mail@shhymmt.com>
2021-01-13 14:44:38 +0900

    #196 memoインターフェイスを追加

---
+++ src/app/interfaces/memo.ts
@@ -0,0 +1,11 @@
+export interface Memo {
+  memoId: string;
+  thumbnailUrl: string;
+  title: string;
+  tags: string[];
+  text: string;
+  isPublic: boolean;
+  likeCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
