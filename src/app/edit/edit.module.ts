44434a15483e68093a3f059b9cf7d7b10f561a6a
shhymmt <mail@shhymmt.com>
2021-02-02 09:54:19 +0900

    #209 0201MTG前まで

--- src/app/edit/edit.module.ts
+++ src/app/edit/edit.module.ts
@@ -7,8 +7,9 @@ import { QuillModule } from 'ngx-quill';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { CropperModule } from '@deer-inc/ngx-croppie';
 
 @NgModule({
   declarations: [EditComponent],
@@ -21,7 +22,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     FormsModule,
     MatSnackBarModule,
-    MatButtonModule
+    MatButtonModule,
+    CropperModule,
   ],
 })
 export class EditModule {}
