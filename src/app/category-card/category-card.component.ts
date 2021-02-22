a90228c8c277d8a43fa0eece2eb564a4c58448f0
shhymmt <mail@shhymmt.com>
2021-02-22 15:07:24 +0900

    0222

--- src/app/category-card/category-card.component.ts
+++ src/app/category-card/category-card.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Memo } from '../interfaces/memo';
+import { MemoService } from '../services/memo.service';
 
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.scss']
+  styleUrls: ['./category-card.component.scss'],
 })
 export class CategoryCardComponent implements OnInit {
+  memoId: string;
+  memo$: Observable<Memo>;
+  constructor(
+    private route: ActivatedRoute,
+    private memoService: MemoService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
