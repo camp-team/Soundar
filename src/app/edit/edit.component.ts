d86b85454329ef954fe1e2e442f48e29fb53ac1e
shhymmt <mail@shhymmt.com>
2021-03-08 17:40:24 +0900

    edit.component.tsのcategoryControl、isPublicControlは使われていないので削除

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -42,11 +42,6 @@ export class EditComponent implements OnInit {
     return this.form.get('text') as FormControl;
   }
 
-  // categoryControlに、categoryのformControlNameを返す
-  get categoryControl(): FormControl {
-    return this.form.get('category') as FormControl;
-  }
-
   // isPublicControlに、isPublicのformControlNameを返す
   get isPublicControl(): FormControl {
     return this.form.get('isPublic') as FormControl;
@@ -58,6 +53,7 @@ export class EditComponent implements OnInit {
     width: 420,
     resultType: 'base64', // base64 | blob
   };
+
   onCroppedImage(image: string): void {
     this.imageFile = image; // image
   }
@@ -80,7 +76,12 @@ export class EditComponent implements OnInit {
     const sendData: Omit<
       // sendDataを、uid,title,text,isPublic,categoriesと定義
       Memo,
-      'memoId' | 'createdAt' | 'updatedAt' | 'likeCount' | 'thumbnailUrl' | 'random'
+      | 'memoId'
+      | 'createdAt'
+      | 'updatedAt'
+      | 'likeCount'
+      | 'thumbnailUrl'
+      | 'random'
     > = {
       uid: this.authService.uid,
       title: formData.title,
