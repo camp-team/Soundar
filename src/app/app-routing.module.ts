36078866b8314bd9bc58c9c2df805285b1ca0508
shhymmt <mail@shhymmt.com>
2021-03-08 16:11:25 +0900

    app.routingのusershellのルーティングはいらない

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,82 +8,20 @@ const routes: Routes = [
       import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
   },
   {
-    path: 'about',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
     path: 'enter',
     loadChildren: () =>
       import('./enter/enter.module').then((m) => m.EnterModule),
   },
-  {
-    path: 'faq',
-    loadChildren: () => import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: 'list',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: 'magazine',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: 'magazine-viewer',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then(
-        (m) => m.UserShellModule
-      ),
-  },
-  {
-    path: 'memo',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: '404',
-    loadChildren: () =>
-      import('./not-found/not-found.module').then((m) => m.NotFoundModule),
-  },
-  {
-    path: 'privacy',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
+
   {
     path: 'search',
     loadChildren: () =>
       import('./search/search.module').then((m) => m.SearchModule),
   },
   {
-    path: 'terms',
+    path: '404',
     loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: 'transaction-law',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then(
-        (m) => m.UserShellModule
-      ),
-  },
-  {
-    path: 'user',
-    loadChildren: () => import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
-  },
-  {
-    path: 'users',
-    loadChildren: () =>
-      import('./user-shell/user-shell.module').then((m) => m.UserShellModule),
+      import('./not-found/not-found.module').then((m) => m.NotFoundModule),
   },
 ];
 
