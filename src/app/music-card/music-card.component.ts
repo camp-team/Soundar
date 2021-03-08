df5c52a0de290d9bf82628bb4e8aea2f34f1867b
shhymmt <mail@shhymmt.com>
2021-03-08 16:51:21 +0900

    music-card.component.tsのメソッドにも空行

--- src/app/music-card/music-card.component.ts
+++ src/app/music-card/music-card.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-music-card',
   templateUrl: './music-card.component.html',
-  styleUrls: ['./music-card.component.scss']
+  styleUrls: ['./music-card.component.scss'],
 })
 export class MusicCardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
   like(): void {
     alert('like!');
   }
