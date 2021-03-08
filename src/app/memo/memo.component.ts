b3911ef47d839423ebcc4fb4d04ecdce0a24000a
shhymmt <mail@shhymmt.com>
2021-03-08 17:36:33 +0900

    memo.component.tsのngOnInitの部分は何もしていないので削除

--- src/app/memo/memo.component.ts
+++ src/app/memo/memo.component.ts
@@ -4,19 +4,11 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-memo',
   templateUrl: './memo.component.html',
-  styleUrls: ['./memo.component.scss']
+  styleUrls: ['./memo.component.scss'],
 })
 export class MemoComponent implements OnInit {
-
   // memoIdに応じたURLを取得するには、ActivatedRouteを使う
-  constructor(
-    private route: ActivatedRoute
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(paramMap => {
-      // alert(paramMap.get('id')); // memo-routingの:idと紐付いている
-    });
-  }
-
+  ngOnInit(): void {}
 }
