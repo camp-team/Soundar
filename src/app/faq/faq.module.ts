86b22cfa2d20e27f9d7e3d3261bb92c5405c5ed7
shhymmt <mail@shhymmt.com>
2021-03-09 16:53:18 +0900

    about.header系のコンポーネントは分けずに、ページのコンポーネントに含める

--- src/app/faq/faq.module.ts
+++ src/app/faq/faq.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { FaqRoutingModule } from './faq-routing.module';
-import { FaqComponent } from './faq.component';
-import { FaqHeaderComponent } from '../faq-header/faq-header.component';
+import { NgModule } from '@angular/core';
 import { FaqContentsComponent } from '../faq-contents/faq-contents.component';
 import { SharedModule } from '../shared/shared.module';
+import { FaqRoutingModule } from './faq-routing.module';
+import { FaqComponent } from './faq.component';
 
 @NgModule({
-  declarations: [FaqComponent, FaqContentsComponent, FaqHeaderComponent],
+  declarations: [FaqComponent, FaqContentsComponent],
   imports: [CommonModule, FaqRoutingModule, SharedModule],
 })
 export class FaqModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { AboutCardComponent } from '../about-card/about-card.component';
+import { UserCardComponent } from '../user-card/user-card.component';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
-import { UserHeaderComponent } from '../user-header/user-header.component';
-import { UserCardComponent } from '../user-card/user-card.component';
-import { AboutCardComponent } from '../about-card/about-card.component';
-
 
 @NgModule({
-  declarations: [UserComponent, UserHeaderComponent, UserCardComponent, AboutCardComponent],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [UserComponent, UserCardComponent, AboutCardComponent],
+  imports: [CommonModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
