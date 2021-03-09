7ef051ed334405d11e03903d95052a1e86604364
shhymmt <mail@shhymmt.com>
2021-03-09 16:58:09 +0900

    about.header系のコンポーネントは分けずに、ページのコンポーネントに含める

--- src/app/transaction-law/transaction-law.module.ts
+++ src/app/transaction-law/transaction-law.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { TransactionLawContentsComponent } from '../transaction-law-contents/transaction-law-contents.component';
 import { TransactionLawRoutingModule } from './transaction-law-routing.module';
 import { TransactionLawComponent } from './transaction-law.component';
-import { SharedModule } from '../shared/shared.module';
-import { TransactionLawHeaderComponent } from '../transaction-law-header/transaction-law-header.component';
-import { TransactionLawContentsComponent } from '../transaction-law-contents/transaction-law-contents.component';
-
 
 @NgModule({
-  declarations: [TransactionLawComponent, TransactionLawHeaderComponent, TransactionLawContentsComponent],
-  imports: [
-    CommonModule,
-    TransactionLawRoutingModule,
-    SharedModule
-  ]
+  declarations: [TransactionLawComponent, TransactionLawContentsComponent],
+  imports: [CommonModule, TransactionLawRoutingModule, SharedModule],
 })
-export class TransactionLawModule { }
+export class TransactionLawModule {}
