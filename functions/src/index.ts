18ce94a769d5fa6cbc6386b9d66fcd6cf322b2eb
shhymmt <mail@shhymmt.com>
2021-03-09 11:19:16 +0900

    functionのindex.tsのコメントを削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 
 import * as admin from 'firebase-admin';
 
-
 // Cloud Functionsでデータベースにアクセスできるようにする
 admin.initializeApp();
 const db = admin.firestore();
@@ -14,18 +13,10 @@ export const createUser = functions.auth.user().onCreate((user) => {
     name: user.displayName,
     avatarURL: user.photoURL,
     email: user.email,
-    createdAt: new Date()
+    createdAt: new Date(),
   });
 });
-
 
 export const deleteUser = functions.auth.user().onDelete((user) => {
   return db.doc('users/${user.uid}').delete();
 });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
