6fe760fdb689d010b82afb60b4208ee2e14a309d
shhymmt <mail@shhymmt.com>
2021-03-09 11:07:18 +0900

    shared.module→shared.routing.moduleを削除して、importにRouterModuleを追加する

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedRoutingModule } from './shared-routing.module';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthorComponent } from '../author/author.component';
 import { HeaderGuideComponent } from '../header-guide/header-guide.component';
 import { MemoCardComponent } from '../memo-card/memo-card.component';
 import { NextCardComponent } from '../next-card/next-card.component';
 import { NextPageButtonComponent } from '../next-page-button/next-page-button.component';
-import { AuthorComponent } from '../author/author.component';
 import { RemoveHtmlTagsPipe } from '../remove-html-tags.pipe';
-import { RouterModule } from '@angular/router';
+import { SharedRoutingModule } from './shared-routing.module';
 
 @NgModule({
   declarations: [
