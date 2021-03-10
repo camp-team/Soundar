8691cf882d05123c67df880c25e8339f1de8c73a
shhymmt <mail@shhymmt.com>
2021-03-10 12:33:34 +0900

    magazine-viewer.htmlのチャプターもforで回す

--- src/app/magazine-viewer/magazine-viewer.component.ts
+++ src/app/magazine-viewer/magazine-viewer.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./magazine-viewer.component.scss'],
 })
 export class MagazineViewerComponent implements OnInit {
+  activeIndex: number;
+  chapters = [
+    { name: 'chapter01', title: 'はじめに' },
+    { name: 'chapter02', title: '続いて' },
+    { name: 'chapter03', title: 'さらにくわしく' },
+    { name: 'chapter04', title: 'ところで' },
+    { name: 'chapter05', title: 'このような別のテクニックもあります' },
+    { name: 'chapter06', title: 'まとめ' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
