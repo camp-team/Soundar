0fb396e6e32b76051fd333297bfd458e94c6ba43
shhymmt <mail@shhymmt.com>
2021-03-11 15:44:02 +0900

    category-card.componentをループで表示させるように修正

--- src/app/category-card/category-card.component.ts
+++ src/app/category-card/category-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -13,16 +14,29 @@ import { MemoService } from '../services/memo.service';
 export class CategoryCardComponent implements OnInit {
   memoId: string;
   memo$: Observable<Memo>;
+  uid: string;
+
+  // カテゴリーをfor文で回すために、categoriesを定義する
+  categories: string[];
+
   constructor(
     private route: ActivatedRoute,
-    private memoService: MemoService
+    private memoService: MemoService,
+    private db: AngularFirestore
   ) {}
 
   ngOnInit(): void {
     this.memo$ = this.route.paramMap.pipe(
       switchMap((param) => {
-        const id = param.get('id');
-        this.memoId = id;
+        // idパラメータを取得
+        const id = param.get('id'); // idを、URLのidと定義して
+        this.memoId = id; // memoIdに、そのidを代入する
+        // カテゴリを取得する
+        const catRef = this.db.collection('memos').doc(id); // memosコレクションの、このURLをidにもつドキュメントを、catRefとする
+        catRef.get().subscribe((docSnapshot) => {
+          const categories = docSnapshot.get('categories'); // docSnapshotのcategoriesフィールドをcategoriesと定義する
+          this.categories = categories; // categoriesの中身を、categoriesに代入する
+        });
         return this.memoService.getMemo(id);
       })
     );
